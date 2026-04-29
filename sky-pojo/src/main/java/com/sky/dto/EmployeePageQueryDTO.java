5ee94474ad38935b1ab27dca771eae8ffe802b88
mx111 <2186698496@qq.com>
2026-04-29 17:58:51 +0800

    员工分页查询业务代码开发

--- sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeePageQueryDTO.java
@@ -1,9 +1,12 @@
 package com.sky.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class EmployeePageQueryDTO implements Serializable {
 
