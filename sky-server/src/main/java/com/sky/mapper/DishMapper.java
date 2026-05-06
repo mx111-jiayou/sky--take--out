098a2a430d4bf6f634de1ca005f97ee5c8d0b74d
mx111 <2186698496@qq.com>
2026-05-06 22:06:29 +0800

    菜品分页查询业务功能

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -18,4 +21,10 @@ public interface DishMapper {
     Integer countByCategoryId(Long categoryId);
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
+/**
+     * 菜品分页 查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,9 +1,18 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
 /**
  * 新增菜品和对应的口味
  */
 public interface DishService {
     public void saveWithFlavor(DishDTO dishDTO);
+/**
+     * 分页查询菜品
+ * @param dishPageQueryDTO
+ * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
