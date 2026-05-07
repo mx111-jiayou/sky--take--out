62edc7a704ca6a4f25f66d511d0ba3d4cc0c450d
mx111 <2186698496@qq.com>
2026-05-07 20:18:30 +0800

    店铺营业状态业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,34 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Api(tags = "店铺相关接口")
+@Slf4j
+public class ShopController {
+    public static final String KEY = "SHOP_STATUS";
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 获取营业状态
+     * @return
+     */
+    @ApiOperation("获取营业状态")
+    @GetMapping ("/status")
+    public Result<Integer> getStatus() {
+        //log.info("获取店铺营业状态");
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        //判断当前店铺营业状态是否存在
+        log.info("获取店铺营业状态为：{}", status == 1?"营业中" : "打烊中");
+        return Result.success(status);
+    }
+}
